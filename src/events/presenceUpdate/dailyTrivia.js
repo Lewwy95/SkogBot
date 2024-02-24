9e40617f321f83618cbb9591f5ee2117e7d0ff6a
Lewwy <johnstonelewwy@gmail.com>
2024-02-24 12:05:13 +0000

    Version 3.1.8
    - Daily Trivia now tracks correct answers.

--- src/events/presenceUpdate/dailyTrivia.js
+++ src/events/presenceUpdate/dailyTrivia.js
@@ -31,14 +31,35 @@ module.exports = async (oldMember, newMember) => {
                     },
                     {
                         name: 'Answer',
-                        value: 'The answer will be revealed in **30 minutes**.'
+                        value: 'The answer will be revealed in **30 minutes** and must be an exact match.'
                     }
                 )
             ]
         });
 
-        setTimeout(function() {
-            triviaMessage.reply({ content: `The correct answer is **${data.results[0].correct_answer}**!` });
+        let correctMembers = [];
+        let correctString = `These are the members that got it right:`;
+
+        setTimeout(async function() {
+            await channel.messages.fetch({ limit: 100 }).then(messages => {
+                messages.forEach(message => {
+                    const msg = message.content.toLowerCase();
+
+                    if (msg.includes(data.results[0].correct_answer.toLowerCase()) && !correctMembers.includes(message.author.id)) {
+                        correctMembers.push(message.author.id);
+                        correctString += `\n<@${message.author.id}>`;
+                    }
+                });
+            });
+
+            if (correctMembers.length === 0 || correctMembers === undefined) {
+                correctString = 'No member had managed to this question correctly.';
+            }
+
+            triviaMessage.reply({
+                content: `The correct answer is **${data.results[0].correct_answer}**!\n\n${correctString}`,
+                allowedMentions: { users: [] }
+            });
         }, 1800000);
 
         await query.updateOne({ timestamp: Date.now() });
