1f573675d0671402a02b332044a69895793bc322
satchi5 <ellysegeddes@gmail.com>
2024-06-05 21:59:16 +0100

    Mindreader Categories
    First draft of mindreader categories. The categories after the large country section need to be expanded upon but can be added to as and when.

---
+++ src/mindreader/categoryArrays.js
@@ -0,0 +1,438 @@
+//Don't like the category? Don't think it fits? Its gone! List begins with countries. I have a comment for after countries so I can keep track of how many categories there are vs the country categories. Please update if you add anything new!
+
+// Country Arrays
+countryA = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Azerbaijan"
+]
+module.exports = countryA;
+countryB = [
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi"
+]
+module.exports = countryB;
+countryC = [
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Columbia",
+    "Comoros",
+    "Costa Rica",
+    "Cote d'Ivoire",
+    "Croatia",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic"
+]
+module.exports = countryC;
+countryD = [
+    "Democratic Republic of Congo",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic"
+]
+module.exports = countryD;
+countryE = [
+    "East Timor",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "England",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia"
+]
+module.exports = countryE;
+countryF = [
+    "Federated States of Micronesia",
+    "Fiji",
+    "Finland",
+    "France"
+]
+module.exports = countryF;
+countryG = [
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Grenada",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana"
+]
+module.exports = countryG;
+countryH = [
+    "Haiti",
+    "Honduras",
+    "Hungary"
+]
+module.exports = countryH;
+countryI = [
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy"
+]
+module.exports = countryI;
+countryJ = [
+    "Jamaica",
+    "Japan",
+    "Jordan"
+]
+module.exports = countryJ;
+countryK = [
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Kosovo",
+    "Kuwait",
+    "Kyrgyzstan"
+]
+module.exports = countryK;
+countryL = [
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg"
+]
+module.exports = countryL;
+countryM = [
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar"
+]
+module.exports = countryM;
+countryN = [
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "North Korea",
+    "North Macedonia",
+    "Northern Ireland",
+    "Norway"
+]
+module.exports = countryN;
+countryP = [
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal"
+]
+module.exports = countryP;
+countryR = [
+    "Republic of Congo",
+    "Romania",
+    "Russia",
+    "Rwanda"
+]
+module.exports = countryR;
+countryS = [
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Scotland",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Korea",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syria"
+]
+module.exports = countryS;
+countryT = [
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Tuvalu"
+]
+module.exports = countryT;
+countryU = [
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan"
+]
+module.exports = countryU;
+countryV = [
+    "Vanautu",
+    "Vatican City",
+    "Venezuela",
+    "Vietnam"
+]
+module.exports = countryV;
+countryZ = [
+    "Zambia",
+    "Zimbabwe"
+]
+module.exports = countryZ;
+//Misc category arrays - 12/21. (Other Categories/Country Categories) Please update first number if adding/removing a category below this point. Thank you!
+//Planets of the Solar System
+planets = [
+    "Venus",
+    "Earth",
+    "Mercury",
+    "Jupiter",
+    "Mars",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+]
+console.exports = planets;
+//Playable Races in Bethesda's Elder Scrolls Franchise
+elderScrollsRaces = [
+    "Altmer",
+    "Bosmer",
+    "Dunmer",
+    "Argonian",
+    "Khajiit",
+    "Redguard",
+    "Breton",
+    "Orsimer",
+    "Nord",
+    "Imperial"
+]
+console.exports = elderScrollsRaces;
+//Games that have won The Game Award for Game of the Year
+gameAward = [
+    "Dragon Age: Inquisition",
+    "The Witcher 3: Wild Hunt",
+    "Legend of Zelda: Breath of the Wild",
+    "God of War",
+    "Sekiro: Shadows Die Twice",
+    "The Last of Us Part II",
+    "It Takes Two",
+    "Elden Ring",
+    "Baldur's Gate 3"
+]
+console.exports = gameAward;
+//Simpsons Characters with over 1000 lines
+simpsons = [
+    "Homer Simpson",
+    "Marge Simpson",
+    "Bart Simpson",
+    "Lisa Simpson",
+    "C. Montgomery Burns",
+    "Moe Syzslak",
+    "Seymour Skinner",
+    "Ned Flanders",
+    "Grampa Simpson",
+    "Milhouse Van Houten",
+    "Chief Wiggum",
+    "Krusty the Clown",
+    "Nelson Muntz",
+    "Lenny Leonard",
+    "Apu Nahasapeemapetilon",
+    "Waylon Smithers"
+]
+console.exports = simpsons;
+//Football teams that have won the UEFA Cup/ Europa League - by number of times each team has won
+//Thrice
+uefaThrice = [
+    "Inter",
+    "Liverpool",
+    "Juventus",
+    "Atlético"
+]
+console.exports = uefaThrice;
+//Twice
+uefaTwice = [
+    "Mönchengladbach",
+    "Tottenham",
+    "Feyenoord",
+    "IFK Göteborg",
+    "Real Madrid",
+    "Parma",
+    "Porto",
+    "Chelsea",
+    "Eintracht Frankfurt"
+]
+console.exports = uefaTwice;
+//Once
+uefaOnce = [
+    "Anderlecht",
+    "Ajax",
+    "Manchester United",
+    "PSV Eindhoven",
+    "Ipswich Town",
+    "Leverkusen",
+    "Napoli",
+    "Bayern",
+    "Schalke",
+    "Galatasaray",
+    "Valencia",
+    "CSKA Moskva",
+    "Zenit",
+    "Shakhtar",
+    "Villareal",
+    "Atalanta"
+]
+console.exports = uefaOnce;
+//Top 20 Best Movies of All Time as reviewed by Rotten Tomatoes
+bestMovies = [
+    "L.A. Confidential",
+    "The Godfather",
+    "Seven Samurai",
+    "Parasite",
+    "Schindler's List",
+    "Top Gun: Maverick",
+    "Toy Story 2",
+    "Chinatown",
+    "On the Waterfront",
+    "The Battle of Algiers",
+    "Toy Story",
+    "Rear Window",
+    "Modern Times",
+    "How to Train your Dragon",
+    "All About Eve",
+    "Spirited Away",
+    "Up",
+    "The Third Man",
+    "Spotlight"
+]
+console.exports = bestMovies;
+//10 Supernatural Characters with the Highest Episode Count (I'm happy to change this to another show - just chose this as example because I like it)
+spnCharacters = [
+    "Sam Winchester",
+    "Dean Winchester",
+    "Castiel",
+    "Lucifer",
+    "Crowley",
+    "Bobby Singer",
+    "Mary Winchester",
+    "Jack Kline",
+    "Rowena Macleod",
+    "Arthur Ketch"
+]
+console.exports = spnCharacters;
+//Artists who have sold over 40 million copies of an album
+fortymilArtists = [
+    "Michael Jackson",
+    "AC/DC",
+    "Whitney Houston",
+    "Pink Floyd",
+    "Eagles",
+    "Meat Loaf",
+    "Shania Twain",
+    "Fleetwood Mac",
+    "Bee Gees"
+]
+console.exports = fortymilArtists;
+//10 richest people of all time
+richPeople = [
+    "Genghis Khan",
+    "Bill Gates",
+    "Alan Rufus", //aka Alan the Red
+    "John D. Rockefeller",
+    "Andrew Carnegie",
+    "Jospeh Stalin",
+    "Akbar I",
+    "Emperor Shenzong", //song shenzong
+    "Augustus Caesar",
+    "Mansa Musa"
+]
+console.exports = richPeople;
+//Regular milk chocolate M&M colours
+mandmColours = [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Blue",
+    "Green",
+    "Brown"
+]
+console.exports = mandmColours;
